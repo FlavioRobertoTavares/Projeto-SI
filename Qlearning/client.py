7e180a6812829dcae0107df19f1874e106234b55
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-06-27 16:29:35 -0300

    Coisas iniciais

--- Qlearning/client.py
+++ Qlearning/client.py
@@ -1,1 +1,30 @@
-#Aqui vocês irão colocar seu algoritmo de aprendizado
+import connection as game
+import subprocess
+import time
+import os
+
+#Executa o jogo de forma automatica no Windowns
+pasta = os.path.dirname(os.path.abspath(__file__))
+atalho= os.path.join(pasta, 'Game.lnk')
+startupinfo = subprocess.STARTUPINFO()
+startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+process = subprocess.Popen(['cmd', '/c', 'start', '', atalho], startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, close_fds=True, shell=True)
+
+#Inicia o jogo
+start = input("Aperte enter para iniciar o algoritmo")
+pular, esquerda, direita, socket = "jump", "left", "right", game.connect(2037)
+
+#Inicio do Qlearning
+estado, recompensa = game.get_state_reward(socket, pular)
+time.sleep(1) #Se botar um movimento seguido do outro sem esperar nenhum tempo, um bugzinho ocorre
+estado, recompensa = game.get_state_reward(socket, pular)
+time.sleep(1)
+estado, recompensa = game.get_state_reward(socket, pular)
+time.sleep(1)
+estado, recompensa = game.get_state_reward(socket, pular)
+time.sleep(1)
+estado, recompensa = game.get_state_reward(socket, pular)
+time.sleep(1)
+
+#Fecha a conexão com o jogo
+socket.close()
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Qlearning
-Execute o arquivo "Aprendizagem por reforço.exe" para iniciar o jogo.
- 
+## Para jogar com o Qlearning
+Execute o arquivo "cliente.py" e aperte enter quando o jogo tiver iniciado
+
+## Para jogar manualmente
+Execute o atalho "Game.lnk" na pasta "Qlearning" para iniciar o jogo. 
+Por terminal: Invoke-Item -Path "Qlearning\Game.lnk"
+O jogo está na versão de Windows.
+
